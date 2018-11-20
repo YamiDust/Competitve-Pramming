76379a184cf87b0ae4f76ebf6dd8c6a53419cff9
issedragon <45211305+issedragon@users.noreply.github.com>
2018-11-20 12:30:36 -0600

    Update main.cpp

--- Proyecto/main.cpp
+++ Proyecto/main.cpp
@@ -1,1 +1,16 @@
+#include <bits/stdc++.h>
 
+using namespace std;
+enum Tipo = {PEON_N, PEON_U, CABALLO, REINA, REY, TORRE, VACIO};
+struct pieza(){
+  Tipo Pieza;
+  bool Color;
+}
+
+int mover(){
+  
+}
+
+int main(){
+  return 0;
+}
