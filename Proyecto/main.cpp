f8c7fdd383fc8455cd1fbb64a64dc0874f44864c
YamiDust <39309305+YamiDust@users.noreply.github.com>
2018-11-20 12:38:06 -0600

    Update main.cpp

--- Proyecto/main.cpp
+++ Proyecto/main.cpp
@@ -1,16 +1,25 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-enum Tipo = {PEON_N, PEON_U, CABALLO, REINA, REY, TORRE, VACIO};
-struct pieza(){
+
+enum Tipo {PEON_N, PEON_U, CABALLO, REINA, REY, TORRE, VACIO};
+
+struct pieza {
   Tipo Pieza;
   bool Color;
-}
+};
+
+pieza **tablero= new pieza* [8];
 
 int mover(){
-  
+
 }
 
 int main(){
+
+    for (int i=0;i<8;i++) {
+        tablero[i]=new pieza[8];
+    }
+
   return 0;
 }
